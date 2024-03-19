ac0f1e2a9efc148b199282cd5438e3779e3306ba
Joshua Ssemwanga <jssem001@plattsburgh.edu>
2024-03-19 21:09:54 +0300

    arithmatic lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+// Write your code her
 
-// Write your code here
+let num1= 2;
+let num2= 31;
+let num3= 9;
+let num4= 5;
+let multiply = num1*num2;
+let random = Math.floor(Math.random()*10)+ 1;
+let mod = num3%num4;
+let max = Math.max(12,11,14,20,19,2);
+//console.log(random);
